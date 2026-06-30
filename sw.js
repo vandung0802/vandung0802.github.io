7d9ea1359b2acf5a57ab5fd701a8c7c564d6f8e5
vandung0802 <vandung0802@gmail.com>
2026-06-30 20:49:45 +0700

    chore: bump sw VERSION 20260630-2 de trigger auto-update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260630-1';
+const VERSION = '20260630-2';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
