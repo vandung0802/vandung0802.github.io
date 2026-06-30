2a99419602e385015f5bb614047d709f0267ddb2
vandung0802 <vandung0802@gmail.com>
2026-06-30 21:03:39 +0700

    feat: them STT vao bang Tóm tat (Đa chuyen, Cho duyet, Tu choi)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260630-4';
+const VERSION = '20260630-5';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
