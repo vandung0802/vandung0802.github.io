5538878083dfff2c5f9c8f6f9ebcf974d25940a0
vandung0802 <vandung0802@gmail.com>
2026-06-30 20:55:37 +0700

    fix: dung controllerchange thay vi message de auto-reload khi co SW moi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260630-2';
+const VERSION = '20260630-3';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
