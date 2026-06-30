36776883af60bcc69896f57e8e5aa29949b3ddc6
vandung0802 <vandung0802@gmail.com>
2026-06-30 21:18:32 +0700

    fix: loai khoi 'Da duyet cho chuyen' nhung mon da chuyen du tien du status=approved

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260630-6';
+const VERSION = '20260630-7';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
