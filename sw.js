d709afdfec1af1235491e5c181bb5eb6941d99c2
vandung0802 <vandung0802@gmail.com>
2026-06-30 21:21:33 +0700

    fix: them auto-reload qua ETag check, nang APP_VERSION len v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260630-7';
+const VERSION = '20260630-8';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
